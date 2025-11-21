45b232f832bfd6d80990837a205938cacb8b59bf
MohamedFares09 <mofares010@gmail.com>
2025-11-22 00:12:10 +0200

    test code with out clean arch

--- lib/feature/auth/login/presentation/login_view.dart
+++ lib/feature/auth/login/presentation/login_view.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hungry_app/core/networking/api/dio_consumer.dart';
+import 'package:hungry_app/feature/auth/login/presentation/cubit/login_cubit.dart';
 import 'package:hungry_app/feature/auth/login/presentation/widget/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -12,10 +16,12 @@ class LoginView extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       body: GestureDetector(
         onTap: () {
-        
           FocusManager.instance.primaryFocus?.unfocus();
         },
-        child: LoginViewBody(),
+        child: BlocProvider(
+          create: (context) => LoginCubit(DioConsumer(Dio())),
+          child: LoginViewBody(),
+        ),
       ),
     );
   }
--- lib/feature/auth/login/presentation/widget/login_view_body.dart
+++ lib/feature/auth/login/presentation/widget/login_view_body.dart
@@ -1,92 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hungry_app/core/constants/app_colors.dart';
 import 'package:hungry_app/core/constants/app_image.dart';
 import 'package:hungry_app/core/widgets/custom_button.dart';
 import 'package:hungry_app/core/widgets/custom_text_filed.dart';
+import 'package:hungry_app/feature/auth/login/presentation/cubit/login_cubit.dart';
+import 'package:hungry_app/feature/auth/login/presentation/cubit/login_state.dart';
 import 'package:hungry_app/feature/auth/login/presentation/widget/do_not_have_an_account.dart';
-import 'package:hungry_app/root.dart';
 
 class LoginViewBody extends StatelessWidget {
-  LoginViewBody({super.key});
-
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey();
+  const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 250),
-          SvgPicture.asset(
-            Assets.svgTextSplashImage,
-            color: AppColors.primaryColor,
-          ),
-          SizedBox(height: 10),
-          const Text(
-            "Welcome Back to Hungry App",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: AppColors.primaryColor,
-            ),
-          ),
-          SizedBox(height: 100),
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
+    final cubit = context.watch<LoginCubit>();
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccess) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(const SnackBar(content: Text("Login Successfully")));
+        }
+        if (state is LoginError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.error)));
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: cubit.formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 250),
+              SvgPicture.asset(
+                Assets.svgTextSplashImage,
                 color: AppColors.primaryColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+              ),
+              SizedBox(height: 10),
+              const Text(
+                "Welcome Back to Hungry App",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primaryColor,
                 ),
               ),
+              SizedBox(height: 100),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: AppColors.primaryColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
 
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 100),
-                      CustomTextFiled(
-                        hintText: 'Email',
-                        controller: emailController,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 100),
+                          CustomTextFiled(
+                            hintText: 'Email',
+                            controller: cubit.emailController,
+                          ),
+                          const SizedBox(height: 20),
+                          CustomTextFiled(
+                            obscureText: true,
+                            hintText: 'Password',
+                            controller: cubit.passwordController,
+                          ),
+                          const SizedBox(height: 16),
+                          BlocBuilder<LoginCubit, LoginState>(
+                            builder: (context, state) {
+                              if (state is LoginLoading) {
+                                return const CircularProgressIndicator();
+                              }
+                              return CustomButton(
+                                text: "Login",
+                                backGroundColor: Colors.white,
+                                color: AppColors.primaryColor,
+                                onTap: () {
+                                  if (cubit.formKey.currentState!.validate()) {
+                                    cubit.login();
+                                  } 
+                                },
+                                width: double.infinity,
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 30),
+                          const DoNoHaveAnAccount(),
+                        ],
                       ),
-                      const SizedBox(height: 20),
-                      CustomTextFiled(
-                        obscureText: true,
-                        hintText: 'Password',
-                        controller: passwordController,
-                      ),
-                      const SizedBox(height: 16),
-                      CustomButton(
-                        text: "Login",
-                        backGroundColor: Colors.white,
-                        color: AppColors.primaryColor,
-                        onTap: () {
-                          if (formKey.currentState!.validate()) {
-                            Navigator.pushReplacementNamed(
-                              context,
-                              Root.routeName,
-                            );
-                          }
-                        },
-                        width: double.infinity,
-                      ),
-                      const SizedBox(height: 30),
-                      const DoNoHaveAnAccount(),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
