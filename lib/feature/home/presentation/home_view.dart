238e485d15df73ac1fffa827ffbe6f34f2c1aecf
MohamedFares09 <mofares010@gmail.com>
2025-11-25 18:11:32 +0200

    add git it

--- lib/feature/home/presentation/home_view.dart
+++ lib/feature/home/presentation/home_view.dart
@@ -1,4 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hungry_app/core/di/di.dart';
+import 'package:hungry_app/core/networking/api/dio_consumer.dart';
+import 'package:hungry_app/feature/home/data/data_sources/home_remote_data_sources.dart';
+import 'package:hungry_app/feature/home/data/repos/home_reposotory_impl.dart';
+import 'package:hungry_app/feature/home/doman/usecases/home_usecases.dart';
+import 'package:hungry_app/feature/home/presentation/cubit/product_cubit.dart';
 import 'package:hungry_app/feature/home/presentation/widget/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,7 +19,12 @@ class HomeView extends StatelessWidget {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: Scaffold(body: HomeViewBody()),
+      child: Scaffold(
+        body: BlocProvider(
+          create: (context) => getIt<ProductCubit>()..getProducts(),
+          child: HomeViewBody(),
+        ),
+      ),
     );
   }
 }
