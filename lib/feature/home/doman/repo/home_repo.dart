c0be5f134a4616938f99656a363e90bcf4951e36
MohamedFares09 <mofares010@gmail.com>
2025-11-25 13:27:07 +0200

    add home repo

---
+++ lib/feature/home/doman/repo/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:hungry_app/core/networking/errors/faliuer.dart';
+import 'package:hungry_app/feature/home/doman/entities/home_entity.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faliuer, List<HomeEntity>>> getHomeData();
+}
